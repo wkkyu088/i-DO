ed8a708d2771160882976d24296e07a49222537c
wkkyu088 <dnjsrbwls08@gmail.com>
2022-09-12 12:59:41 +0900

    constants 색상 추가

--- lib/constants.dart
+++ lib/constants.dart
@@ -52,67 +52,44 @@ String kFailureIcon = '❓';
 
 // create_modal 관련
 final List<bool> days = [true, false, false];
-final List<bool> checked = [
-  false, // 1
-  false, // 2
-  false, // 3
-  false, // 4
-  false, // 5
-  false, // 6
-  false, // 7
-  false, // 8
-  false, // 9
-  false, // 10
-  false, // 11
-  false, // 12
-  false, // 13
-  false, // 14
-  false, // 15
-  false, // 16
-  false, // 17
-  false, // 18
-  false, // 19
-  false, // 20
-  false, // 21
-  false, // 22
-  false, // 23
-  false, // 24
-  false, // 25
-  false, // 26
-  false, // 27
-  false, // 28
-  false, // 29
-  false, // 30
-];
+List<bool> checked = List.filled(colorChart.length, false);
 final List colorChart = [
   [const Color(0xFF7DA8FF), const Color(0xFFCCDCFD)], // 1
   [const Color(0xFFa1c4fd), const Color(0xFFc2e9fb)], // 2
-  [const Color(0xFFa6c1ee), const Color(0xFFfbc2eb)], // 3
-  [const Color(0xFFace0f9), const Color(0xFFfff1eb)], // 4
-  [const Color(0xFFe0c3fc), const Color(0xFF8ec5fc)], // 5
-  [const Color(0xFF8989ba), const Color(0xFFa7a6cb)], // 6
-  [const Color(0xFF875ACC), const Color.fromARGB(255, 195, 172, 230)], // 7
-  [const Color(0xFF84fab0), const Color(0xFF8fd3f4)], // 8
-  [const Color(0xFF96e6a1), const Color(0xFFf9f586)], // 9
-  [const Color(0xFF96fbc4), const Color(0xFFd4fc79)], // 10
-  [const Color(0xFFc1dfc4), const Color(0xFdeecddF)], // 11
+  [const Color(0xFF3A66BE), const Color(0xFFB3C9F4)], // 3
+  [const Color(0xFF213B5D), const Color(0xFF6692CB)], // 4
+  [const Color(0xFF9BB6E3), const Color(0xFFE5CCEB)], // 5
+  [const Color(0xFFD6B9F1), const Color(0xFFA3CFFC)], // 6
+  [const Color(0xFF8989ba), const Color(0xFFa7a6cb)], // 7
+  [const Color(0xFF8C65AC), const Color(0xFFCFB8E0)], // 8
+  [const Color(0xFFA6DADE), const Color(0xFFFFDBD3)], // 9
+  [const Color(0xFF53A693), const Color(0xFF8BDEC2)], // 10
+  [const Color(0xFF65CCB8), const Color(0xFFace0f9)], // 11
   [const Color(0xFF00cdac), const Color(0xFF8ddad5)], // 12
-  [const Color(0xFF7CAC5D), const Color.fromARGB(255, 186, 220, 164)], // 13
-  [const Color(0xFFebbba7), const Color(0xFFcfc7f8)], // 14
-  [const Color(0xFFF8A14B), const Color.fromARGB(255, 250, 213, 176)], // 15
-  [const Color(0xFFfcb69f), const Color(0xFFffecd2)], // 16
-  [const Color(0xFFfda085), const Color(0xFFf6d365)], // 17
-  [const Color(0xFFff9a9e), const Color(0xFFfad0c4)], // 18
-  [const Color(0xFFdf9a9e), const Color(0xFFdad0c4)], // 19
-  [const Color(0xFFe96443), const Color(0xFF904e95)], // 20
-  [const Color(0xFFEC7B97), const Color.fromARGB(255, 255, 203, 216)], // 21
-  [const Color(0xFFf5576c), const Color(0xFFFBB7C5)], // 22
-  [const Color(0xFFc79081), const Color(0xFFdfa579)], // 23
-  [const Color(0xFF005aa7), const Color(0xFFfffde4)], // 24
-  [const Color(0xFF13547A), const Color(0xFF80d0c7)], // 25
-  [const Color(0xFF093637), const Color(0xFF44a08d)], // 26
-  [const Color(0xFF330867), const Color(0xFF30cfd0)], // 27
-  [const Color(0xFF09203f), const Color(0xFF537895)], // 28
-  [const Color(0xFF1e130c), const Color(0xFF9a8478)], // 29
-  [const Color(0xFF596164), const Color(0xFF868f96)], // 30
+  [const Color(0xFF00ABC0), const Color(0xFFE8E6E1)], // 13
+  [const Color(0xFF13547A), const Color(0xFF80d0c7)], // 14
+  [const Color(0xFF95E29E), const Color(0xFFF1ED86)], // 15
+  [const Color(0xFF60C98F), const Color(0xFFCEF378)], // 16
+  [const Color(0xFF7CAC5D), const Color(0xFFBADCA4)], // 17
+  [const Color(0xFF1A4119), const Color(0xFF8CC28B)], // 18
+  [const Color(0xFF99BA9E), const Color(0xFCE4C2D5)], // 19
+  [const Color(0xFFebbba7), const Color(0xFFcfc7f8)], // 20
+  [const Color(0xFFdf9a9e), const Color(0xFFdad0c4)], // 21
+  [const Color(0xFFff9a9e), const Color(0xFFfad0c4)], // 22
+  [const Color(0xFFEC7B97), const Color(0xFFFFCBD8)], // 23
+  [const Color(0xFFFC5D9D), const Color(0xFFF8C596)], // 24
+  [const Color(0xFFF03750), const Color(0xFFF8B1BF)], // 25
+  [const Color(0xFFE94343), const Color(0xFFBA709D)], // 26
+  [const Color(0xFFD95A80), const Color(0xFF6763AD)], // 27
+  [const Color(0xFF891431), const Color(0xFFE6A1B9)], // 28
+  [const Color(0xFFfcb69f), const Color(0xFFFBE4C3)], // 29
+  [const Color(0xFFF8A14B), const Color(0xFFFAD5B0)], // 30
+  [const Color(0xFFFFAA01), const Color(0xFFFFED4B)], // 31
+  [const Color(0xFFED504B), const Color(0xFFFFCA76)], // 32
+  [const Color(0xFF843C35), const Color(0xFFF4C0A8)], // 33
+  [const Color(0xFFB58274), const Color(0xFFdfa579)], // 34
+  [const Color(0xFF573E2D), const Color(0xFFC7A18C)], // 35
+  [const Color(0xFF808384), const Color(0xFFCED4D8)], // 36
+  [const Color(0xFF4C5255), const Color(0xFF868f96)], // 37
+  [const Color(0xFF0F0F0D), const Color(0xFFBE1829)], // 38
 ];
